e37604337ed3065a0b20e8105af9f115c2fdb929
Arunvasuacchu <arunvasur@gmail.com>
2025-12-26 11:17:39 +0530

    Add exportPDF stub

--- src/utils/exportPDF.js
+++ src/utils/exportPDF.js
@@ -1,25 +1,4 @@
-import { PDFDocument, rgb } from "pdf-lib";
-
-export async function exportPdf(file, canvases) {
-  const buffer = await file.arrayBuffer();
-  const pdfDoc = await PDFDocument.load(buffer);
-  const pages = pdfDoc.getPages();
-
-  pages.forEach((page, index) => {
-    const canvas = canvases[index + 1];
-    if (!canvas) return;
-
-    canvas.getObjects().forEach((obj) => {
-      if (obj.text) {
-        page.drawText(obj.text, {
-          x: obj.left,
-          y: page.getHeight() - obj.top,
-          size: obj.fontSize || 12,
-          color: rgb(0, 0, 0),
-        });
-      }
-    });
-  });
-
-  return await pdfDoc.save();
+export async function exportPDF(file, canvases) {
+  const arrayBuffer = await file.arrayBuffer();
+  return new Uint8Array(arrayBuffer);
 }
